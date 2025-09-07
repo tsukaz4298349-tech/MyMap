24345c3749aa7e7d37b0578b222da2c0519647cf
中尾司 <tsukaz4298349@gmail.com>
2025-09-07 21:31:39 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 中尾司 on 2025/09/07.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
