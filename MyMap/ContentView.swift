0d581e8b0438571be64214ed1f766341dbe96319
“N.T” <tsukaz4298349@gmail.com>
2025-09-20 22:02:25 +0900

    “ファーストコミット”

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    //入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    //検索キーワードを保持する状態変数、初期値は"東京駅"
+    @State var displaySearchKey: String = "東京駅"
+    //マップ種類　最初は標準から
+    @State var displayMapType: MapType = .standard
+    
     var body: some View {
+        //垂直にレイアウト
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            //テキストフィールド(文字入力)
+            TextField("キーワード", text: $inputText, prompt:
+                Text("キーワードを入力してくださ"))
+            //入力が完了された時
+                .onSubmit {
+                    //入力が完了したので検索キーワードに設定する
+                    displaySearchKey = inputText
+            }
+            //余白を追加
+                .padding()
+            
+            //奥から手前方向にレイアウト　(右下基準で配置する)
+            ZStack(alignment: .bottomTrailing) {
+                //マップを表示
+                MapView(searchKey: displaySearchKey, mapType: displayMapType)
+                
+                //マップ種類切り替えボタン
+                Button {
+                    //標準 -> 衛生写真 -> 衛生写真+交通機関ラベル
+                    if displayMapType == .standard {
+                        displayMapType = .satelite
+                    } else if displayMapType == .satelite {
+                        displayMapType = .hybrid
+                    } else {
+                        displayMapType = .standard
+                    }
+                } label: {
+                    //マップアイコンの表示
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0)
+                }//Buttonここまで
+                //右の余白を20あける
+                .padding(.trailing, 20.0)
+                //下の余白30あける
+                .padding(.bottom, 30.0)
+                
+            }//ZStackここまで
+        }//VStackここまで
     }
 }
 
---
+++ MyMap/MapView.swift
@@ -0,0 +1,86 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by 中尾司 on 2025/09/07.
+//
+
+import SwiftUI
+import MapKit
+
+//画面で選択したマップの種類を示す列挙型
+enum MapType {
+    case standard    //標準
+    case satelite    //衛生写真
+    case hybrid      //衛生写真+交通機関ラベル
+}
+
+struct MapView: View {
+    //検索キーワード
+    let searchKey: String
+    //マップ種類
+    let mapType: MapType
+    
+    //キーワードから取得した緯度経度
+    @State var targetCoordinate = CLLocationCoordinate2D()
+    //表示するマップの位置
+    @State var cameraPosition: MapCameraPosition = .automatic
+    //表示するマップのスタイル
+    var mapStyle: MapStyle {
+        switch mapType {
+        case .standard:
+            return MapStyle.standard()
+        case .satelite:
+            return MapStyle.imagery()
+        case .hybrid:
+            return MapStyle.hybrid()
+        }
+    }
+    
+    var body: some View {
+        //マップを表示
+        Map(position: $cameraPosition){
+            //マップにピンを表示
+            Marker(searchKey, coordinate: targetCoordinate)
+        }
+        //マップのスタイルを指定
+        .mapStyle(mapStyle)
+        //検索キーワードの変更を検知
+        .onChange(of: searchKey, initial: true) { oldValue, newValue in
+            //入力されたキーワードをデバックエリアに表示
+            print("検索キーワード：\(newValue)")
+            
+            //地図の検索クエリ(命令)の作成
+            let request = MKLocalSearch.Request()
+            //検索クエリにキーワードの設定
+            request.naturalLanguageQuery = newValue
+            //MKLocalSearchの初期化
+            let search = MKLocalSearch(request: request)
+            
+            //検索の開始
+            search.start { response, error in
+                //結果が存在する時は、1件目を取り出す
+                if let mapItems = response?.mapItems,
+                   let mapItem = mapItems.first {
+                    
+                    //位置情報から緯度経度をtargetCoordinateに取り出す
+                    targetCoordinate = mapItem.placemark.coordinate
+                    
+                    //緯度経度をデバックエリアに表示
+                    print("緯度経度:\(targetCoordinate)")
+                    
+                    //表示するマップの領域を作成
+                    cameraPosition = .region(MKCoordinateRegion(
+                        center: targetCoordinate,
+                        latitudinalMeters: 500.0,
+                        longitudinalMeters: 500.0
+                    ))
+                }//ifここまで
+            }//search.startここまで
+        }//onChangeここまで
+    }//bodyここまで
+}//MapViewここまで
+
+#Preview {
+    MapView(searchKey: "東京駅", mapType: .standard)
+}
